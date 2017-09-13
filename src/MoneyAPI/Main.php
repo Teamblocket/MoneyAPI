6347ccc175840960c6dd08351d91cb75c8fb0a61
Angel <bubblicious712@gmail.com>
2017-09-13 15:22:28 -0400

    Create Main.php

---
+++ src/MoneyAPI/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MoneyAPI;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase{
+
+}
