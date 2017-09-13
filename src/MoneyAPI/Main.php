8a5b4e3865754ac60659bde2f7982586268a1398
Angel <bubblicious712@gmail.com>
2017-09-13 15:29:42 -0400

    Update Main.php

--- src/MoneyAPI/Main.php
+++ src/MoneyAPI/Main.php
@@ -8,5 +8,36 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
 class Main extends PluginBase{
-
+  
+  public function onEnable(){
+    if($this->configExists()){
+      $this->saveConfig();
+    } else {
+      $this->createConfig();
+    }
+  }
+  
+  public function createConfig(){
+    if(!file_exists($this->getDataFolder() . "prefs.yml")){
+      $this->config = new Config($this->getDataFolder() . "prefs.yml", Config::YAML, [
+        "Max-Money" => 100000000
+        ]);
+    }
+  }
+  
+  public function saveConfig(){
+    if(file_exists($this->getDataFolder() . "prefs.yml")){
+      $this->config->save();
+    }
+  }
+  
+  public function configExists(){
+    if(file_exists($this->getDataFolder() . "prefs.yml")){
+      return true;
+    }
+  }
+  
+  public function getMoneyManager(string $username){
+    return new MoneyManager($this, $username);
+  }
 }
